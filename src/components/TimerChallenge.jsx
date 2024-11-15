06ab20e645b5a7e8803af9a79b7fd9ee7a700acc
seetaramoruganti <orugantiseetaram@gmail.com>
2024-11-15 04:40:05 -0500

    added timer challenge component

---
+++ src/components/TimerChallenge.jsx
@@ -0,0 +1,14 @@
+export default function TimerChallenge({ title, targetTime }) {
+  return (
+    <section className="challenge">
+      <h2>{title}</h2>
+      <p className="challenge-time">
+        {targetTime} second{targetTime > 1 ? "s" : ""}
+      </p>
+      <p>
+        <button>Start Challenge</button>
+      </p>
+      <p className="">Timer Inactive .... /// Timer is running...</p>
+    </section>
+  );
+}
