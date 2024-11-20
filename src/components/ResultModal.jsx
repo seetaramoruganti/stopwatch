db72d4f041ed7f0fe5bd6db137f1a43bc68c0ad4
seetaramoruganti <orugantiseetaram@gmail.com>
2024-11-20 08:46:46 -0500

    Esc key to close dialog panel

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -20,7 +20,7 @@ const ResultModal = forwardRef(function ResultModal(
   });
 
   return createPortal(
-    <dialog ref={dialog} className="result-modal">
+    <dialog ref={dialog} className="result-modal" onClose={onReset}>
       {userLost && <h2>You lost</h2>}
       {!userLost && <h2>Your Score: {score}</h2>}
       <p>
