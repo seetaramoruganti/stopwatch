7c91b00bb95edd28f0984ed62645e2424c2e1fc0
seetaramoruganti <orugantiseetaram@gmail.com>
2024-11-20 07:47:21 -0500

    changed the use of use Timer to setInterval to check if the clock is active or expired every 10milli sec.chaging the dialog screen and computing the percentage of time left with the gamer clock

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,125 +1,41 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
+import { createPortal } from "react-dom";
 
 const ResultModal = forwardRef(function ResultModal(
-  { result, targetTime },
+  { targetTime, remainingTime, onReset },
   ref
 ) {
   const dialog = useRef();
 
+  const userLost = remainingTime <= 0;
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
+
   useImperativeHandle(ref, () => {
     return {
       open() {
-        // dialog.current.showModal();
+        dialog.current.showModal();
       },
     };
   });
 
-  return (
+  return createPortal(
     <dialog ref={dialog} className="result-modal">
-      <h2>You {result}</h2>
+      {userLost && <h2>You lost</h2>}
+      {!userLost && <h2>Your Score: {score}</h2>}
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
       <p>
-        You stopped the timer with <strong>X seconds left.</strong>
+        You stopped the timer with{" "}
+        <strong>{formattedRemainingTime} seconds left.</strong>
       </p>
-      <form method="dialog">
+      <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
-    </dialog>
+    </dialog>,
+    document.getElementById("modal")
   );
 });
 
 export default ResultModal;
-
-// In the solution, as a first step, a ref is created inside the Form component. This ref is then "connected" to the <form> element and will be used for calling the form's reset() method whenever the (to-be-added) clear() method will be called from inside some other component:
-
-// import React from 'react';
-
-// export default function Form(props, ref) {
-//   const form = React.useRef();
-
-//   return (
-//     <form ref={form}>
-//       <p>
-//         <label>Name</label>
-//         <input type="text" />
-//       </p>
-
-//       <p>
-//         <label>Email</label>
-//         <input type="email" />
-//       </p>
-//       <p id="actions">
-//         <button>Save</button>
-//       </p>
-//     </form>
-//   );
-// }
-
-// To expose a clear() method that can be called from inside the App component, React's useImperativeHandle Hook must be used. This Hook allows you (= the developer) to expose an API (a collection of methods) from a component to other components.
-
-// This useImperativeHandle Hook needs a (forwarded) ref (coming from the component that wants to access the exposed API) as a first input value.
-
-// As a second argument, it expects to get a function that must return an object that contains all methods that should be exposed.
-
-// Therefore, the Form component is also wrapped with forwardRef.
-
-// Inside the object returned in the second argument function of useImperativeHandle, a clear() method is added. Inside that method, the form ref is used to access the JavaScript form object (belonging to <form>) and call its reset() method.
-
-// import React from 'react';
-
-// const Form = React.forwardRef(function Form(props, ref) {
-//   const form = React.useRef();
-//   React.useImperativeHandle(ref, () => {
-//     return {
-//       clear() {
-//         form.current.reset();
-//       },
-//     };
-//   });
-
-//   return (
-//     <form ref={form}>
-//       <p>
-//         <label>Name</label>
-//         <input type="text" />
-//       </p>
-
-//       <p>
-//         <label>Email</label>
-//         <input type="email" />
-//       </p>
-//       <p id="actions">
-//         <button>Save</button>
-//       </p>
-//     </form>
-//   );
-// });
-
-// export default Form;
-
-// Finally, the App component is edited to call that clear() method from inside handleRestart.
-
-// A new ref (form) is created and passed via the special ref prop to the custom Form component.
-
-// That form ref is then used to call the Form component's clear() method:
-
-// import React from 'react';
-
-// import Form from './Form';
-
-// export function App() {
-//   const form = React.useRef();
-
-//   function handleRestart() {
-//     form.current.clear();
-//   }
-
-//   return (
-//     <div id="app">
-//       <button onClick={handleRestart}>Restart</button>
-//       <Form ref={form} />
-//     </div>
-//   );
-// }
--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -10,46 +10,74 @@ import ResultModal from "./ResultModal.jsx";
 // one second timer and five second timer and stop the five second timer after
 //one second this challenge is failed as both the components are using the same timer
 export default function TimerChallenge({ title, targetTime }) {
-  const [timerStarted, setTimerStarted] = useState(false);
-  const [timerExpired, setTimerExpired] = useState(false);
+  // const [timerStarted, setTimerStarted] = useState(false);
+  // const [timerExpired, setTimerExpired] = useState(false);
 
-  //   let timer;
-  //Using the timer inside the function and outside the handle start and the handle stop solves
-  // the issue of passing the timer from one function to other function, but As the state variables
-  // are changed, the component execution starts again, so then the timer will never be stopped even
-  // if there and stop Execute
+  // //   let timer;
+  // //Using the timer inside the function and outside the handle start and the handle stop solves
+  // // the issue of passing the timer from one function to other function, but As the state variables
+  // // are changed, the component execution starts again, so then the timer will never be stopped even
+  // // if there and stop Execute
+
+  // const timer = useRef();
+  // const dialog = useRef();
+
+  // function handleStart() {
+  //   timer.current = setTimeout(() => {
+  //     setTimerExpired(true);
+  //     dialog.current.open();
+  //   }, targetTime * 1000);
+
+  //   setTimerStarted(true);
+  // }
 
   const timer = useRef();
   const dialog = useRef();
 
-  function handleStart() {
-    timer.current = setTimeout(() => {
-      setTimerExpired(true);
-      dialog.current.open();
-    }, targetTime * 1000);
+  const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
 
-    setTimerStarted(true);
+  const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
+
+  if (timeRemaining <= 0) {
+    clearInterval(timer.current);
+    dialog.current.open();
+  }
+
+  function handleReset() {
+    setTimeRemaining(targetTime * 1000);
+  }
+
+  function handleStart() {
+    timer.current = setInterval(() => {
+      setTimeRemaining((prevTimeRemaining) => prevTimeRemaining - 10);
+    }, 10);
   }
 
   function handleStop() {
-    clearTimeout(timer.current);
+    dialog.current.open();
+    clearInterval(timer.current);
   }
 
   return (
     <>
-      <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
+      <ResultModal
+        ref={dialog}
+        targetTime={targetTime}
+        remainingTime={timeRemaining}
+        onReset={handleReset}
+      />
       <section className="challenge">
         <h2>{title}</h2>
         <p className="challenge-time">
           {targetTime} second{targetTime > 1 ? "s" : ""}
         </p>
         <p>
-          <button onClick={timerStarted ? handleStop : handleStart}>
-            {timerStarted ? "Stop" : "Start"} Challenge
+          <button onClick={timerIsActive ? handleStop : handleStart}>
+            {timerIsActive ? "Stop" : "Start"} Challenge
           </button>
         </p>
-        <p className={timerStarted ? "active" : undefined}>
-          {timerStarted ? "Time is running..." : "Timer inactive"}
+        <p className={timerIsActive ? "active" : undefined}>
+          {timerIsActive ? "Time is running..." : "Timer inactive"}
         </p>
       </section>
     </>
